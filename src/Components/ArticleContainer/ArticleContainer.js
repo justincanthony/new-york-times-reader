8315466e67ba1201dbbdf23c60a02893e619bf40
Justin Anthony <justincanthony@gmail.com>
2021-11-01 23:05:38 -0700

    Set data to state and render cards

--- src/Components/ArticleContainer/ArticleContainer.js
+++ src/Components/ArticleContainer/ArticleContainer.js
@@ -4,19 +4,25 @@ import './ArticleContainer.css';
 import { ArticleCard } from '../ArticleCard/ArticleCard';
 import { getTopStories } from '../../apiCalls';
 
+let newsCards;
+
 export const ArticleContainer = () => {
-  const [stories, setStories] = useState([]);
+  const [topStories, setTopStories] = useState({});
   const [error, setError] = useState('');
 
   const getStories = () => {
     getTopStories()
-      .then((data) => setStories(data))
+      .then((data) => setTopStories(data))
       .catch((err) => setError(err));
   };
 
   useEffect(() => {
     getStories();
   }, []);
+
+  const newsCards = () => {
+    topStories.results.map((story) => <ArticleCard article={story} />);
+  };
 
   return <Box component="section">{newsCards}</Box>;
 };
