e8f00c4b7e8ca7715cd3c562acb46a841b1d0b64
Justin Anthony <justincanthony@gmail.com>
2021-11-01 22:36:19 -0700

    Add container and hook

---
+++ src/Components/ArticleContainer/ArticleContainer.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import './ArticleContainer.css';
+
+export const ArticleContainer = () => {
+  const [stories, setStories] = useState([]);
+
+  return <div></div>;
+};
