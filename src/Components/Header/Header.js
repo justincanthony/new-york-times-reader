30de756329e3a3f3ccbf0f083d00ccda1c66dd87
Justin Anthony <justincanthony@gmail.com>
2021-11-01 18:51:28 -0700

    Remove link styling

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import './Header.css';
+import './Header.css';
 import { Link } from 'react-router-dom';
 import { AppBar } from '@mui/material';
 import Box from '@mui/material/Box';
