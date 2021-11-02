d9c74148f75104383a4b1f86ac5a7281ffee0263
Justin Anthony <justincanthony@gmail.com>
2021-11-01 23:17:39 -0700

    Set stories state and render cards

--- src/Components/ArticleContainer/ArticleContainer.js
+++ src/Components/ArticleContainer/ArticleContainer.js
@@ -3,16 +3,17 @@ import { Box } from '@mui/system';
 import './ArticleContainer.css';
 import { ArticleCard } from '../ArticleCard/ArticleCard';
 import { getTopStories } from '../../apiCalls';
+import { v4 as uuidv4 } from 'uuid';
 
 let newsCards;
 
 export const ArticleContainer = () => {
-  const [topStories, setTopStories] = useState({});
+  const [topStories, setTopStories] = useState([]);
   const [error, setError] = useState('');
 
   const getStories = () => {
     getTopStories()
-      .then((data) => setTopStories(data))
+      .then((data) => setTopStories(data.results))
       .catch((err) => setError(err));
   };
 
@@ -20,9 +21,9 @@ export const ArticleContainer = () => {
     getStories();
   }, []);
 
-  const newsCards = () => {
-    topStories.results.map((story) => <ArticleCard article={story} />);
-  };
+  newsCards = topStories.map((story) => (
+    <ArticleCard key={uuidv4()} article={story} />
+  ));
 
-  return <Box component="section">{newsCards}</Box>;
+  return <section component="section">{newsCards}</section>;
 };
