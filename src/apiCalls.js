46a80429b38236638d856818eb4a82a209adc9ee
Justin Anthony <justincanthony@gmail.com>
2021-11-01 19:20:39 -0700

    Add api key

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,12 @@
 export const getTopStories = async (section) => {
-  const response = await fetch();
+  section = 'home';
+  const response = await fetch(
+    `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=H7gdiWL0eP6A5389w0R1HGa8rgBO7rvu`,
+    {
+      headers: {
+        Accept: 'application/json',
+      },
+    }
+  );
   return await response.json();
 };
