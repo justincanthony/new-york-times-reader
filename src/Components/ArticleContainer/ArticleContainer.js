9161c76bd4d1d90bc5488d7073ef9a7a6e98766b
Justin Anthony <justincanthony@gmail.com>
2021-11-01 22:49:53 -0700

    Add fetch call

--- src/Components/ArticleContainer/ArticleContainer.js
+++ src/Components/ArticleContainer/ArticleContainer.js
@@ -1,8 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Box } from '@mui/system';
 import './ArticleContainer.css';
+import { ArticleCard } from '../ArticleCard/ArticleCard';
+import { getTopStories } from '../../apiCalls';
 
 export const ArticleContainer = () => {
   const [stories, setStories] = useState([]);
+  const [error, setError] = useState('');
 
-  return <div></div>;
+  const getStories = () => {
+    getTopStories()
+      .then((data) => setStories(data))
+      .catch((err) => setError(err));
+  };
+
+  useEffect(() => {
+    getStories();
+  }, []);
+
+  return <Box component="section">{newsCards}</Box>;
 };
