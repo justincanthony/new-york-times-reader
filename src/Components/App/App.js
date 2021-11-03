fdbd1f30f79299ad064ffb20a62ee85b95d48e18
Justin Anthony <justincanthony@gmail.com>
2021-11-03 15:54:49 -0600

    Add funciton to add id to stories

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,19 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
 import { ArticleContainer } from '../ArticleContainer/ArticleContainer';
 import { getTopStories } from '../../apiCalls';
 import { SingleStoryContainer } from '../SingleStoryContainer/SingleStoryContainer';
 import { Header } from '../Header/Header';
 import './App.css';
+import { FullStoryCard } from '../FullStoryCard/FullStoryCard';
 
 const App = () => {
   const [topStories, setTopStories] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState('');
 
+  const addID = (data) => {
+    return data.map((story) => ({ ...story, id: uuidv4() }));
+  };
+
   const getStories = () => {
     getTopStories()
-      .then((data) => setTopStories(data.results))
+      .then((data) => setTopStories(addID(data.results)))
       .catch((err) => setError(err));
   };
 
@@ -35,18 +41,20 @@ const App = () => {
           path="/"
           render={() => (
             <React.Fragment>
-              {!isLoading && topStories && (
-                <ArticleContainer topStories={topStories} />
-              )}
-              {isLoading && !error && <p>Loading...</p>}
+              {!isLoading && <ArticleContainer topStories={topStories} />}
+              {isLoading && <p>Loading...</p>}
               {!isLoading && error && <p>whoops something went wrong</p>}
             </React.Fragment>
           )}
         />
         <Route
-          path="/article/:title"
+          exact
+          path="/article/:id"
           render={({ match }) => {
-            return <SingleStoryContainer title={match.params.title} />;
+            const story = topStories.find(
+              (article) => article.id === match.params.id
+            );
+            return <FullStoryCard story={story} />;
           }}
         />
         {/* <Route render={() => <ErrorPage />} /> */}
