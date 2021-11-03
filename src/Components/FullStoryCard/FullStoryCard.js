cf92fcb5a05da9518ef27d23baac157dd7eae283
Justin Anthony <justincanthony@gmail.com>
2021-11-02 19:55:43 -0700

    Create  and render full story card

--- src/Components/FullStoryCard/FullStoryCard.js
+++ src/Components/FullStoryCard/FullStoryCard.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import './FullStoryCard.css';
+import Card from '@mui/material/Card';
+import Typography from '@mui/material/Typography';
+import CardContent from '@mui/material/CardContent';
 
 export const FullStoryCard = ({ story }) => {
-  console.log(story);
-  return <div></div>;
+  return (
+    <Card variant="outlined">
+      <CardContent>
+        <Typography variant="h5">{story.title}</Typography>
+        <Typography sx={{ fontSize: 16 }} color="text.secondary" gutterBottom>
+          <p>{story.abstract}</p>
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 };
