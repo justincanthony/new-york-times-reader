6781a2718026307efc5af4719c781a3afdac24cc
Justin Anthony <justincanthony@gmail.com>
2021-11-03 15:53:26 -0600

    Remove fetch call

--- src/Components/SingleStoryContainer/SingleStoryContainer.js
+++ src/Components/SingleStoryContainer/SingleStoryContainer.js
@@ -3,27 +3,7 @@ import './SingleStoryContainer.css';
 import { getTopStories } from '../../apiCalls';
 import { FullStoryCard } from '../FullStoryCard/FullStoryCard';
 
-export const SingleStoryContainer = ({ title }) => {
-  console.log(title);
-  const [stories, setStories] = useState([]);
-  const [story, setStory] = useState({});
-  const [error, setError] = useState('');
-
-  const getStories = () => {
-    getTopStories()
-      .then((data) => setStories(data.results))
-      .catch((err) => setError(err));
-  };
-
-  useEffect(() => {
-    getStories();
-  }, []);
-
-  useEffect(() => {
-    setStory(stories.find((story) => story.title === title));
-  }, [stories, title]);
-
-  return (
-    <React.Fragment>{story && <FullStoryCard story={story} />}</React.Fragment>
-  );
+export const SingleStoryContainer = ({ story }) => {
+  console.log(story);
+  return <FullStoryCard story={story} />;
 };
