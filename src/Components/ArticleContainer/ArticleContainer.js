80ecf94799550f8529281e82a7e8706aeab519f0
Justin Anthony <justincanthony@gmail.com>
2021-11-03 16:47:11 -0600

    set is home for button rendering

--- src/Components/ArticleContainer/ArticleContainer.js
+++ src/Components/ArticleContainer/ArticleContainer.js
@@ -4,7 +4,8 @@ import './ArticleContainer.css';
 import { ArticleCard } from '../ArticleCard/ArticleCard';
 import { v4 as uuidv4 } from 'uuid';
 
-export const ArticleContainer = ({ topStories }) => {
+export const ArticleContainer = ({ topStories, setIsHome }) => {
+  setIsHome(true);
   const newsCards = topStories.map((story) => (
     <ArticleCard key={uuidv4()} article={story} />
   ));
