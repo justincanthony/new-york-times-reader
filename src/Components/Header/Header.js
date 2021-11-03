5e4181eae77cd6f646008eb0ec66029857bd2878
Justin Anthony <justincanthony@gmail.com>
2021-11-03 16:46:38 -0600

    Add condition to render button

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
 import { AppBar } from '@mui/material';
@@ -7,7 +7,7 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 
-export const Header = () => {
+export const Header = ({ isHome }) => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -15,12 +15,14 @@ export const Header = () => {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             <Link to="/">New York Times Top Stories</Link>
           </Typography>
-          <Button
-            variant="inherit"
-            onClick={() => window.location.reload(false)}
-          >
-            Refresh Stories
-          </Button>
+          {isHome && (
+            <Button
+              variant="inherit"
+              onClick={() => window.location.reload(false)}
+            >
+              Refresh Stories
+            </Button>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
