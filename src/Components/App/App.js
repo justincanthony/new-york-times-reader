88d98925c3b1fbf633b996077d7517da9f604ba1
Justin Anthony <justincanthony@gmail.com>
2021-11-03 12:55:22 -0600

    Move state to app and adjust condtional rendering

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,15 +1,48 @@
+import React, { useState, useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { ArticleContainer } from '../ArticleContainer/ArticleContainer';
+import { getTopStories } from '../../apiCalls';
 import { SingleStoryContainer } from '../SingleStoryContainer/SingleStoryContainer';
 import { Header } from '../Header/Header';
 import './App.css';
 
 const App = () => {
+  const [topStories, setTopStories] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  const getStories = () => {
+    getTopStories()
+      .then((data) => setTopStories(data.results))
+      .catch((err) => setError(err));
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+    getStories();
+  }, []);
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, [topStories]);
+
   return (
     <div className="App">
       <Header />
       <Switch>
-        <Route exact path="/" render={() => <ArticleContainer />} />
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <React.Fragment>
+              {!isLoading && topStories && (
+                <ArticleContainer topStories={topStories} />
+              )}
+              {isLoading && !error && <p>Loading...</p>}
+              {!isLoading && error && <p>whoops something went wrong</p>}
+            </React.Fragment>
+          )}
+        />
         <Route
           path="/article/:title"
           render={({ match }) => {
--- src/Components/ArticleContainer/ArticleContainer.js
+++ src/Components/ArticleContainer/ArticleContainer.js
@@ -1,32 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { Box } from '@mui/system';
+// import { Box } from '@mui/system';
 import './ArticleContainer.css';
 import { ArticleCard } from '../ArticleCard/ArticleCard';
-import { getTopStories } from '../../apiCalls';
 import { v4 as uuidv4 } from 'uuid';
 
-let newsCards;
-
-export const ArticleContainer = () => {
-  const [topStories, setTopStories] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState('');
-
-  const getStories = () => {
-    getTopStories()
-      .then((data) => setTopStories(data.results))
-      .catch((err) => setError(err));
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    getStories();
-    setIsLoading(true);
-  }, []);
-
-  newsCards = topStories.map((story) => (
+export const ArticleContainer = ({ topStories }) => {
+  const newsCards = topStories.map((story) => (
     <ArticleCard key={uuidv4()} article={story} />
   ));
 
-  return <section component="section">{newsCards}</section>;
+  return (
+    <React.Fragment>
+      <section component="section">{newsCards}</section>
+    </React.Fragment>
+  );
 };
