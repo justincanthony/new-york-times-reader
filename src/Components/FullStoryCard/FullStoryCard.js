64ce8277c72d1e28c99db4d41a2475d53ebd8b83
Justin Anthony <justincanthony@gmail.com>
2021-11-03 19:41:35 -0600

    Add details and styling for single article

--- src/Components/FullStoryCard/FullStoryCard.js
+++ src/Components/FullStoryCard/FullStoryCard.js
@@ -1,20 +1,52 @@
 import React from 'react';
 import './FullStoryCard.css';
+import dayjs from 'dayjs';
+
 import { Link } from 'react-router-dom';
 import Card from '@mui/material/Card';
 import { Button } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import CardContent from '@mui/material/CardContent';
+import { CardMedia } from '@mui/material';
 
 export const FullStoryCard = ({ story, setIsHome }) => {
+  const { title, abstract, multimedia, published_date, byline, section, url } =
+    story;
+
   setIsHome(false);
 
   return (
     <Card variant="outlined">
       <CardContent>
-        <Typography variant="h5">{story.title}</Typography>
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          sx={{ background: '#f6f6f6' }}
+        >
+          {section.toUpperCase()}
+        </Typography>
+        <Typography variant="h5">{title}</Typography>
+        <Typography variant="body2" color="text.secondary">
+          {byline}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Published {dayjs(published_date).format('MMM D, YYYY [at] h:mm A')}
+        </Typography>
+        <CardMedia
+          component="img"
+          height="auto"
+          image={multimedia[0].url}
+          alt="{multimedia[0].caption}"
+        />
+        <Typography variant="body2" color="text.secondary" gutterBottom>
+          {multimedia[0].caption}
+        </Typography>
+        <br />
         <Typography sx={{ fontSize: 16 }} color="text.secondary" gutterBottom>
-          <p>{story.abstract}</p>
+          {abstract}
+        </Typography>
+        <Typography sx={{ fontSize: 16 }} color="text.secondary" gutterBottom>
+          To view this story please visit <a href={url}>The New York Times</a>
         </Typography>
       </CardContent>
       <Link to="/">
