49001701c3d82c0caf8995ab6da1575c4c085ced
Justin Anthony <justincanthony@gmail.com>
2021-11-03 20:00:52 -0600

    Add conditional rendering

--- src/Components/FullStoryCard/FullStoryCard.js
+++ src/Components/FullStoryCard/FullStoryCard.js
@@ -10,48 +10,60 @@ import CardContent from '@mui/material/CardContent';
 import { CardMedia } from '@mui/material';
 
 export const FullStoryCard = ({ story, setIsHome }) => {
-  const { title, abstract, multimedia, published_date, byline, section, url } =
-    story;
-
   setIsHome(false);
 
   return (
-    <Card variant="outlined">
-      <CardContent>
-        <Typography
-          variant="body2"
-          color="text.secondary"
-          sx={{ background: '#f6f6f6' }}
-        >
-          {section.toUpperCase()}
-        </Typography>
-        <Typography variant="h5">{title}</Typography>
-        <Typography variant="body2" color="text.secondary">
-          {byline}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Published {dayjs(published_date).format('MMM D, YYYY [at] h:mm A')}
-        </Typography>
-        <CardMedia
-          component="img"
-          height="auto"
-          image={multimedia[0].url}
-          alt="{multimedia[0].caption}"
-        />
-        <Typography variant="body2" color="text.secondary" gutterBottom>
-          {multimedia[0].caption}
-        </Typography>
-        <br />
-        <Typography sx={{ fontSize: 16 }} color="text.secondary" gutterBottom>
-          {abstract}
-        </Typography>
-        <Typography sx={{ fontSize: 16 }} color="text.secondary" gutterBottom>
-          To view this story please visit <a href={url}>The New York Times</a>
-        </Typography>
-      </CardContent>
-      <Link to="/">
-        <Button variant="inherit">Go Back</Button>
-      </Link>
-    </Card>
+    <React.Fragment>
+      {!story && <p>Whoops, something went wrong!</p>}
+      {story && (
+        <Card variant="outlined">
+          <CardContent>
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              sx={{ background: '#f6f6f6' }}
+            >
+              {story.section.toUpperCase()}
+            </Typography>
+            <Typography variant="h5">{story.title}</Typography>
+            <Typography variant="body2" color="text.secondary">
+              {story.byline}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Published{' '}
+              {dayjs(story.published_date).format('MMM D, YYYY [at] h:mm A')}
+            </Typography>
+            <CardMedia
+              component="img"
+              height="auto"
+              image={story.multimedia[0].url}
+              alt="{multimedia[0].caption}"
+            />
+            <Typography variant="body2" color="text.secondary" gutterBottom>
+              {story.multimedia[0].caption}
+            </Typography>
+            <br />
+            <Typography
+              sx={{ fontSize: 16 }}
+              color="text.secondary"
+              gutterBottom
+            >
+              {story.abstract}
+            </Typography>
+            <Typography
+              sx={{ fontSize: 16 }}
+              color="text.secondary"
+              gutterBottom
+            >
+              To view this story please visit{' '}
+              <a href={story.url}>The New York Times</a>
+            </Typography>
+          </CardContent>
+          <Link to="/">
+            <Button variant="inherit">Go Back</Button>
+          </Link>
+        </Card>
+      )}
+    </React.Fragment>
   );
 };
